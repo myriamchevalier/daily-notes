09c765273e7b443b4a366b7c3bf618667f8a5efe
Myriam Chevalier <chevalier.myriam@hotmail.com>
2021-07-16 10:25:07 -0500

    initial commit

---
+++ notes.js
@@ -0,0 +1,18 @@
+const dailyNotes = [
+    {
+        id: 1,
+        subject: "GitHub Basics",
+        date: "July 9th",
+        feeling: "Excited, anticipating what is to come",
+        timeSpent: 6
+    },
+    {
+        id: 2,
+        subject: "GitHub Workflow (team",
+        date: "July 13th and July 14th",
+        feeling: "A little overwhelmed with new information. Much better the next day after my brain processed.",
+        timeSpent: 9
+    }
+]
+
+console.log(dailyNotes);
